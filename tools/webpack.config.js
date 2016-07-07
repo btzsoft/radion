a0687370696a0d15cc59044258ff9a790cf4c39a
Andrei Botezatu <btzsoft@gmail.com>
2016-07-07 15:42:03 +0300

    Removed unused stages from webpack.config.js

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -54,8 +54,6 @@ const config = {
             'react',
             'stage-0',
             'stage-1',
-            'stage-2',
-            'stage-3',
           ],
           plugins: [
             'transform-runtime',
